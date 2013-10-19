cb93979d3831933392ea66ee8ca7112d16a6127a
Paul Stone <pmstone247@gmail.com>
2013-10-18 19:07:46 -0600

    madlibs

---
+++ mad_libs.rb
@@ -0,0 +1,52 @@
+#!/usr/bin/env ruby
+
+def ask_for(question)
+  puts question
+  gets.chomp
+end
+
+
+
+question1 = ask_for("Give me an adjective:")
+question2 = ask_for("Give me another adjective:")
+question3 = ask_for("Give me a noun:")
+question4 = ask_for("Give me another noun:")
+question5 = ask_for("Give me a plural noun:")
+question6 = ask_for("Give me a game:")
+question7 = ask_for("Give me another plural noun:")
+question8 = ask_for("Give me a verb ending in 'ing':")
+question9 = ask_for("Give me another verb ending in 'ing':")
+question10 = ask_for("Give me another plural noun:")
+question11 = ask_for("Give me another verb ending in 'ing':")
+question12 = ask_for("Give me another noun:")
+question13 = ask_for("Give me a plant:")
+question14 = ask_for("Give me a part of the body:")
+question15 = ask_for("Give me a place:")
+question16 = ask_for("Give me another verb ending in 'ing':")
+question17 = ask_for("Give me another adjective")
+question18 = ask_for("Give me a number:")
+question19 = ask_for("Give me another plural noun:")
+
+
+puts "A vacation is when you take a trip to some #{question1} place"
+puts "with your #{question2} family. Usually you go to some place"
+puts "that is near a/an #{question3} or up on a/an #{question4}."
+puts "A good vacation place is one where you can ride #{question5}"
+puts "or play #{question6} or go hunting for #{question7}. I like"
+puts "to spend my time #{question8} or #{question9}."
+puts "When parents go on a vacation, they spend their time eating"
+puts "three #{question10} a day, and fathers play golf, and mothers"
+puts "sit around #{question11}. Last summer, my little brother"
+puts "fell in a/an #{question12} and got poison #{question13} all"
+puts "over his #{question14}. My family is going to go to (the)"
+puts "#{question15}, and I will practice #{question16}. Parents"
+puts "need vacations more than kids because parents are always very"
+puts "#{question17} and because they have to work #{question18}"
+puts "hours every day all year making enough #{question19} to pay"
+puts "for the vacation."
+
+
+
+
+
+
